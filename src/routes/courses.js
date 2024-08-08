fce86e9308ba4580d9b5b7a09bf378b871cf53c6
Nguyen Thai Hieu <87901713+Hiesu19@users.noreply.github.com>
2024-08-08 21:31:21 +0700

    feat: delete courses

--- src/routes/courses.js
+++ src/routes/courses.js
@@ -8,6 +8,7 @@ router.post("/store", coursesController.store);
 router.get("/", coursesController.home_courses);
 router.get("/:id/edit", coursesController.edit);
 router.put("/:id", coursesController.update);
+router.delete("/:id", coursesController.destroy);
 router.get("/:slug", coursesController.show_course);
 
 module.exports = router;
